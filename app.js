92c07c48b9258672fd6167470a628f2194a7af0f
zevolpato <zevolpato@outlook.com>
2024-09-05 17:56:10 +0000

    first

---
+++ app.js
@@ -0,0 +1,99 @@
+console.log(firebase)
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+const userDetails = document.getElementById('userDetails');
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+const db = firebase.firestore();
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+
+    if (user) {
+
+        // Database Reference
+        thingsRef = db.collection('things')
+
+        createThing.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        }
+
+    }
+});
+
+
+
+auth.onAuthStateChanged(user => {
+
+    if (user) {
+
+        // Database Reference
+        thingsRef = db.collection('things')
+
+        // ..... omitted .....
+
+        // Query
+        unsubscribe = thingsRef.where('uid', '==', user.uid)
+            .onSnapshot(querySnapshot => {
+                
+                // Map results to an array of li elements
+
+                const items = querySnapshot.docs.map(doc => {
+
+                    return `<li>${doc.data().name}</li>`
+
+                });
+
+                thingsList.innerHTML = items.join('');
+
+            });
+
+
+
+    } else {
+        // Unsubscribe when the user signs out
+        unsubscribe && unsubscribe();
+    }
+});
+
+thingsRef
+    .where('uid', '==', user.uid)
+    .orderBy('createdAt') // Requires an index
